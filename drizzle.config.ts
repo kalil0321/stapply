be9f4a292df5c32653f1037d1466202c7e72cfa5
kalil0321 <kalil.bouzigues@gmail.com>
2025-09-10 12:55:19 +0200

    new drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    schema: "./src/db/schema.ts",
+    out: "./supabase/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+    schemaFilter: ["public", "better_auth"],
+    verbose: true,
+    strict: true,
+});
