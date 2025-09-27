3be156be2edb697f79d960634bf64cfff7aeb427
kalil0321 <kalil.bouzigues@gmail.com>
2025-09-27 14:32:36 +0200

    remove isSuccess prop from ServerApplicationView in ApplicationPage

--- src/app/(dashboard)/application/[id]/page.tsx
+++ src/app/(dashboard)/application/[id]/page.tsx
@@ -170,7 +170,6 @@ export default function ApplicationPage() {
             <ServerApplicationView
                 application={application}
                 status={status}
-                isSuccess={isSuccess}
                 output={output}
             />
         );
