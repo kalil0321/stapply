78f1c03669097d01e993dd247d86b2d21b64d00c
kalil0321 <kalil.bouzigues@gmail.com>
2025-09-29 04:44:02 +0200

    add autumn provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme";
+import { AutumnProvider } from "autumn-js/react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default async function RootLayout({
             <body
                 className={`flex flex-col items-center justify-center min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                <AutumnProvider betterAuthUrl={process.env.NEXT_PUBLIC_BETTER_AUTH_URL}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
@@ -37,6 +39,7 @@ export default async function RootLayout({
                 >
                     {children}
                 </ThemeProvider>
+                </AutumnProvider>
             </body>
         </html>
     );
